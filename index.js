7da987cea1d683ee516bcb950600fa4975e11d18
yani82 <yani.iman1@gmail.com>
2021-02-09 00:29:12 -0500

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 // Code your solutions in this file
-function writeCards(names, event){
-    let messages = []
-    for(let i = 0; i < names.length; i++){
-        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+function writeCards( namesArray, event ) {
+    let thankYouCards = []
+    for ( let i = 0; i < namesArray.length; i++ ) {
+      thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
     }
-    return messages;
-}
+    return thankYouCards
+  }
 
-function countDown(int){
-    while (int >= 0) {
-        console.log(int);
-        int--;
+  function countDown( startingNumber ) {
+    while ( startingNumber > 0 ) {
+      console.log( startingNumber );
+      startingNumber -= 1;
     }
-}
+    console.log( startingNumber );
+  }
