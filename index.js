d58584f97906bbbe43eb0d8a0e5fc4300cc77693
yani82 <yani.iman1@gmail.com>
2021-04-27 01:15:34 -0400

    commit tree 60e562c2c86aee5c83b7873569d451de9cc961c5

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 function writeCards( namesArray, event ) {
     let thankYouCards = []
     for ( let i = 0; i < namesArray.length; i++ ) {
+      // for (initialization; condition; iteration)
       thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
     }
     return thankYouCards
@@ -9,6 +10,7 @@ function writeCards( namesArray, event ) {
 
   function countDown( startingNumber ) {
     while ( startingNumber > 0 ) {
+      // (conditional) {[loop body]}
       console.log( startingNumber );
       startingNumber -= 1;
     }
